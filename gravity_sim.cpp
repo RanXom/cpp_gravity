f51c19bcc0e7763819c896bb8aaa4fff211db1ea
RanXom <saiyedshizain@gmail.com>
2025-08-15 23:30:53 +0530

    feat: initialise an opengl window and draw a circle on it

--- gravity_sim.cpp
+++ gravity_sim.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <cmath>
+
+float screenWidth = 600.0f;
+float screenHeight = 800.0f;
 
 GLFWwindow* StartGLFW();
+void draw_circle(float centerX, float centerY, float radius, int res) {
+  
+}
 
 int main() {
   GLFWwindow* window = StartGLFW();
-
   float centerX = screenWidth / 2.0f;
   float centerY = screenHeight / 2.0f;
-  float radius = 50.0;
+  float radius = 50.0f;
   int res = 100;
- 
+
   while (!glfwWindowShouldClose(window)) {
+    draw_circle(centerX, centerY, radius, res);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -32,4 +40,27 @@ GLFWwindow* StartGLFW() {
 
   glfwMakeContextCurrent(window);
   return window;
+}
+
+void draw_circle(float centerX, float centerY, float radius, int res) {
+  // Set up orthographic projection (pixel coords)
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, 800, 0, 600, -1, 1);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+ 
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex2f(centerX, centerY);
+
+  for (int i = 0; i <= res; ++i) {
+    float angle = 2.0f * 3.141592653589 * (static_cast<float>(i) / res);
+    float x = centerX + cos(angle) * radius;
+    float y = centerY + sin(angle) * radius;
+    glVertex2f(x, y);
+  }
+
+  glEnd();
 }
